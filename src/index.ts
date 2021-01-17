c945a602e1e5b91d7926557970b701147a013836
daniel <dpletzke@gmail.com>
2021-01-16 22:04:59 -0600

    add post database entity

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,19 @@
-console.log('Hello World');
+import { MikroORM } from "@mikro-orm/core";
+import { __prod__ } from './constants';
+import { Post } from "./entities/Post";
+
+const main = async () => {
+  const orm = await MikroORM.init({
+    entities: [Post],
+    dbName: 'lireddit',
+    type: 'postgresql',
+    user: 'postgres',
+    password: 'postgres',
+    debug: !__prod__
+  });
+
+  const post = orm.em.create(Post, {title: 'My first Post'})
+  await orm.em.persistAndFlush(post);
+}
+
+main();
